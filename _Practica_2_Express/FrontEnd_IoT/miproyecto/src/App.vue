640c9ec512b6c91cc899220ddd82150f986a4da9
root <barthjoss@hotmail.es>
2021-11-24 18:30:22 -0600

    A repo -->> Implementacion de primer proyecto vue y aplicacion de un boton con cambio de mensajes

---
+++ _Practica_2_Express/FrontEnd_IoT/miproyecto/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <br>
+    <button class="button-32" type="button" @click="cambio">Change Message 🔁</button>
+    <HelloWorld :msg="msg"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return{
+      msg: "Bienvenidos a Vue ✌"
+    };
+  },
+  components: {
+    HelloWorld
+  },
+  methods:{
+    cambio(){
+      this.msg = "Hola mundo 🗺"
+    }
+  }
+}
+</script>
+
+<style>
+.button-32 {
+  background-color: #fff000;
+  border-radius: 12px;
+  color: #000;
+  cursor: pointer;
+  font-weight: bold;
+  padding: 10px 15px;
+  text-align: center;
+  transition: 200ms;
+  width: 12%;
+  box-sizing: border-box;
+  border: 0;
+  font-size: 16px;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.button-32:not(:disabled):hover,
+.button-32:not(:disabled):focus {
+  outline: 0;
+  background: #f4e603;
+  box-shadow: 0 0 0 2px rgba(0,0,0,.2), 0 3px 8px 0 rgba(0,0,0,.15);
+}
+
+.button-32:disabled {
+  filter: saturate(0.2) opacity(0.5);
+  cursor: not-allowed;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
